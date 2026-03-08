d76d640257d463b5e88ca399f0c941a36ebc4cb1
Rin <lyfay6768@gmail.com>
2026-03-08 20:32:49 +0300

    added file Home

--- Home.jsx
+++ Home.jsx
@@ -1,7 +1,17 @@
 function Home(){
     return(
         <>
-        
+        <h1>Gojo</h1>
+
+        <div className=" container">
+            <div className=" row">
+                <div className=" col-12">
+                    <div className=" card">
+                        <h1>Gojo</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
